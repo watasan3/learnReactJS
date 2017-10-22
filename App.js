4e428dd792932aec7158e27972cd5f3fe1dbcbf9
teradonburi <daikiterai@gmail.com>
2017-10-22 15:33:23 +0900

    Material UI

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { load } from './user'
 
+import { withStyles } from 'material-ui/styles'
+import { AppBar,Toolbar, Avatar, Card, CardContent, Button, Dialog, DialogTitle, DialogContent } from 'material-ui'
+import Typography from 'material-ui/Typography'
+import { Email } from 'material-ui-icons'
+
+
 // connectのdecorator
 @connect(
   // propsに受け取るreducerのstate
@@ -13,9 +19,28 @@ import { load } from './user'
 )
 export default class App extends React.Component {
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      open:false,
+      user:null,
+    }
+  }
+
   componentWillMount() {
     // user取得APIコールのactionをキックする
     this.props.load()
+  }
+
+  handleClickOpen (user) {
+    this.setState({
+      open: true,
+      user: user,
+    })
+  }
+
+  handleRequestClose () {
+    this.setState({ open: false })
   }
 
   render () {
@@ -24,18 +49,36 @@ export default class App extends React.Component {
     console.log(users)
     return (
       <div>
+          <AppBar position="static" color="primary">
+            <Toolbar>
+              <Typography type="title" color="inherit">
+                タイトル
+              </Typography>
+            </Toolbar>
+          </AppBar>
           {/* 配列形式で返却されるためmapで展開する */}
           {users && users.map((user) => {
             return (
                 // ループで展開する要素には一意なkeyをつける（ReactJSの決まり事）
-                <div key={user.email}>
-                  <img src={user.picture.thumbnail} />
-                  <p>名前:{user.name.first + ' ' + user.name.last}</p>
-                  <p>性別:{user.gender}</p>
-                  <p>email:{user.email}</p>
-                </div>
+                <Card key={user.email} style={{marginTop:'10px'}}>
+                  <CardContent style={{color:'#408040'}}>
+                    <Avatar src={user.picture.thumbnail} />
+                    <p style={{margin:10}}>{'名前:' + user.name.first + ' ' + user.name.last} </p>
+                    <p style={{margin:10}}>{'性別:' + (user.gender == 'male' ? '男性' : '女性')}</p>
+                    <div style={{textAlign: 'right'}} >
+                      <Button onClick={() => this.handleClickOpen(user)}><Email/>メールする</Button>                    
+                    </div>
+                  </CardContent>
+                </Card>    
             )
           })}
+          {
+            this.state.open &&
+            <Dialog open={this.state.open} onRequestClose={() => this.handleRequestClose()}>
+              <DialogTitle>メールアドレス</DialogTitle>
+              <DialogContent>{this.state.user.email}</DialogContent>
+            </Dialog>
+          }  
       </div>
     )
   }
