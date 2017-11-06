b09d160930c37e39dfbf9deadcec5a7ba2c865fd
teradonburi <daikiterai@gmail.com>
2017-11-06 10:27:22 +0900

    redux-form

--- client/components/TodoPage.js
+++ client/components/TodoPage.js
@@ -3,11 +3,34 @@ import { connect } from 'react-redux';
 import { load, add } from 'reducer/user'
 
 import { withStyles } from 'material-ui/styles'
-import { AppBar,Toolbar, Avatar, Card, CardContent, Button, Dialog, DialogTitle, DialogContent } from 'material-ui'
+import { AppBar,Toolbar, Avatar, Card, CardContent, Button, TextField } from 'material-ui'
 import Typography from 'material-ui/Typography'
 import { Email } from 'material-ui-icons'
+import { Field, reduxForm } from 'redux-form'
 
-
+@reduxForm({
+  form: 'syncValidation',
+  validate: values => {
+    
+    const errors = {}
+    if (!values.username) {
+      errors.username = '必須項目です'
+    } 
+    if (!values.email) {
+      errors.email = '必須項目です'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'メールアドレスとして認識できません'
+    }
+    if (!values.age) {
+      errors.age = '必須項目です'
+    } else if (isNaN(Number(values.age))) {
+      errors.age = '数字でありません'
+    } else if (Number(values.age) < 18) {
+      errors.age = '１８歳以上限定です'
+    }
+    return errors
+  }
+})
 export default class TodoPage extends React.Component {
 
   constructor (props) {
@@ -22,21 +45,52 @@ export default class TodoPage extends React.Component {
   handlePageMove(path) {
     this.props.history.push(path)
   }
+  
+
+  submit(values) {
+    // print the form values to the console
+    console.log(values)
+  }
 
   render () {
-    const { todos } = this.props
+    const { handleSubmit, submitting } = this.props
+
+    const renderField = ({
+      input,
+      label,
+      type,
+      meta: { touched, error, warning }
+    }) => {
+      const isError = !!(touched && error)
+      return (
+        <div>
+          <div>
+            <TextField style={{marginTop:5,marginBottom:5}} error={isError} label={label} helperText={isError ? error : ''} {...input} type={type} />
+          </div>
+        </div>
+      )
+    }
+    
     // 初回はnullが返ってくる（initialState）、処理完了後に再度結果が返ってくる
     // console.log(users)
     return (
       <div>
-          <AppBar position="static" color="primary">
-            <Toolbar>
-              <Typography type="title" color="inherit">
-                TODOページ
-              </Typography>
-              <Button style={{color:'#fff',position:'absolute',top:15,right:0}} onClick={()=> this.handlePageMove('/')}>ユーザページへ</Button>
-            </Toolbar>
-          </AppBar>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <Typography type="title" color="inherit">
+              TODOページ
+            </Typography>
+            <Button style={{color:'#fff',position:'absolute',top:15,right:0}} onClick={()=> this.handlePageMove('/')}>ユーザページへ</Button>
+          </Toolbar>
+        </AppBar>
+        <Card style={{padding:10}}>
+          <form onSubmit={handleSubmit(this.submit)}>
+            <Field name="username" type="text" component={renderField} label="ユーザ名" />
+            <Field name="email" type="email" component={renderField} label="メールアドレス" />
+            <Field name="age" type="number" component={renderField} label="年齢" />
+            <Button style={{marginTop:10}} raised type="submit" disabled={submitting}>送信</Button>
+          </form>
+        </Card>
       </div>
     )
   }
