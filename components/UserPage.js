03de3c603e29cde6b22bc8e09e8e596f9962ed5c
teradonburi <daikiterai@gmail.com>
2018-01-07 20:17:34 +0900

    fix

--- components/UserPage.js
+++ components/UserPage.js
@@ -2,9 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { load } from '../reducer/user'
 
-import { withStyles } from 'material-ui/styles'
-import { AppBar, Toolbar, Avatar, Card, CardContent, Button, Dialog, DialogTitle, DialogContent } from 'material-ui'
+import { withTheme, withStyles } from 'material-ui/styles'
+import { AppBar,Toolbar, Avatar, Card, CardContent, Button, Dialog, DialogTitle, DialogContent } from 'material-ui'
 import { Email } from 'material-ui-icons'
+import withWidth from 'material-ui/utils/withWidth'
+import { orange } from 'material-ui/colors'
+
 
 import Intro from '../Intro'
 @Intro([
@@ -32,7 +35,16 @@ import Intro from '../Intro'
   // propsに付与するactions
   { load }
 )
-export default class UserPage extends React.Component {
+@withWidth()
+@withTheme()
+@withStyles({
+  root: {
+    fontStyle: 'italic',
+    fontSize: 21,
+    minHeight: 64,
+  }
+})
+export default class App extends React.Component {
 
   constructor (props) {
     super(props)
@@ -61,42 +73,44 @@ export default class UserPage extends React.Component {
   handlePageMove(path) {
     this.props.history.push(path)
   }
-  
+
   render () {
-    const { users } = this.props
+    const { users, theme, classes, width } = this.props
+    const { primary, secondary } = theme.palette
+
     // 初回はnullが返ってくる（initialState）、処理完了後に再度結果が返ってくる
-    // console.log(users)
+    console.log(users)
     return (
       <div>
-          <AppBar position="static" color="primary">
-            <Toolbar>
-              ユーザページ
-              <Button id='item2' style={{color:'#fff',position:'absolute',top:15,right:0}} onClick={()=> this.handlePageMove('/todo')}>TODOページへ</Button>
-            </Toolbar>
-          </AppBar>
-          {/* 配列形式で返却されるためmapで展開する */}
-          {users && users.map((user) => {
-            return (
-                // ループで展開する要素には一意なkeyをつける（ReactJSの決まり事）
-                <Card key={user.email} style={{marginTop:'10px'}}>
-                  <CardContent style={{color:'#408040'}}>
-                    <Avatar src={user.picture.thumbnail} />
-                    <p style={{margin:10}}>{'名前:' + user.name.first + ' ' + user.name.last} </p>
-                    <p style={{margin:10}}>{'性別:' + (user.gender == 'male' ? '男性' : '女性')}</p>
-                    <div style={{textAlign: 'right'}} >
-                      <Button id='item1' onClick={() => this.handleClickOpen(user)}><Email/>メールする</Button>                    
-                    </div>
-                  </CardContent>
-                </Card>
-            )
-          })}
-          {
-            this.state.open &&
-            <Dialog open={this.state.open} onRequestClose={() => this.handleRequestClose()}>
-              <DialogTitle>メールアドレス</DialogTitle>
-              <DialogContent>{this.state.user.email}</DialogContent>
-            </Dialog>
-          }  
+        <AppBar position="static" color="primary">
+          <Toolbar classes={{root: classes.root}} >
+            タイトル({ width === 'xs' ? 'スマホ' : 'PC'})
+            <Button id='item2' style={{color:'#fff',position:'absolute',top:15,right:0}} onClick={()=> this.handlePageMove('/todo')}>TODOページへ</Button>
+          </Toolbar>
+        </AppBar>
+        {/* 配列形式で返却されるためmapで展開する */}
+        {users && users.map((user) => {
+          return (
+              // ループで展開する要素には一意なkeyをつける（ReactJSの決まり事）
+              <Card key={user.email} style={{marginTop:'10px'}}>
+                <CardContent style={{color:'#408040'}}>
+                  <Avatar src={user.picture.thumbnail} />
+                  <p style={{margin:10, color:primary[500]}}>{'名前:' + user.name.first + ' ' + user.name.last} </p>
+                  <p style={{margin:10, color:secondary[500]}}>{'性別:' + (user.gender == 'male' ? '男性' : '女性')}</p>
+                  <div style={{textAlign: 'right'}} >
+                    <Button id='item1' raised color='accent' onClick={() => this.handleClickOpen(user)}><Email style={{marginRight: 5, color: orange[200]}}/>Email</Button>
+                  </div>
+                </CardContent>
+              </Card>
+          )
+        })}
+        {
+          this.state.open &&
+          <Dialog open={this.state.open} onRequestClose={() => this.handleRequestClose()}>
+            <DialogTitle>メールアドレス</DialogTitle>
+            <DialogContent>{this.state.user.email}</DialogContent>
+          </Dialog>
+        }
       </div>
     )
   }
