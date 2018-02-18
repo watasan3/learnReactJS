0c3eb288fbaaf5d9472e03891891000663d3a183
teradonburi <daikiterai@gmail.com>
2018-02-18 11:48:30 +0900

    redux-devtoolの設定

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React  from 'react'
 import ReactDOM from 'react-dom'
 import createHistory from 'history/createBrowserHistory'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import client from 'axios'
 import thunk from 'redux-thunk'
@@ -11,12 +11,14 @@ import { routerMiddleware } from 'react-router-redux'
 import App from './App'
 import reducer from './reducer/reducer'
 
+// redux-devtoolの設定
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 // ブラウザ履歴保存用のストレージを作成
 const history = createHistory()
 // axiosをthunkの追加引数に加える
 const thunkWithClient = thunk.withExtraArgument(client)
 // redux-thunkをミドルウェアに適用
-const store = createStore(reducer, applyMiddleware(routerMiddleware(history),thunkWithClient))
+const store = createStore(reducer, composeEnhancers(applyMiddleware(routerMiddleware(history),thunkWithClient)))
 
 const render = Component => {
   ReactDOM.render(
