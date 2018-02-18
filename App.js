790b60ec1cbc515b102c3d69515cc7689fc93c19
teradonburi <daikiterai@gmail.com>
2018-02-18 14:28:05 +0900

    refs

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import Rect from './Rect'
 
 export default class App extends React.Component {
+
+  // アップロードされたファイルの処理
+  handleUpload = (e) => {
+    const file = e.target.files[0]
+    const reader = new FileReader()
+    reader.readAsText(file, 'UTF-8')
+    reader.onload = e => {
+      alert(e.target.result)
+    }
+    e.target.value = null
+
+    // refs経由で生のHTMLElementを操作できる
+    this.refs.done.style.display = ''
+  }
 
   render () {
     return (
       <div>
-        <Rect />
-        <Rect num={1} bgcolor='#e02020' />
-        <Rect num={2} bgcolor='#20e020' />
-        <Rect num={3} bgcolor='#2020e0' />
+        {/* ref属性値指定でrefsに登録される */}
+        <input type='file' ref='upload' style={{display: 'none'}} onChange={this.handleUpload} />
+        {/* 別のDOMからrefs参照でクリックイベント処理をキックする */}
+        <button onClick={() => this.refs.upload.click()}>アップロード</button>
+        <div ref='done' style={{display: 'none'}}>アップロード完了</div>
       </div>
     )
   }
