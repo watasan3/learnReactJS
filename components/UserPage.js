ecfa13a4d3e0c3af731298cf543af43109cfcaab
teradonburi <daikiterai@gmail.com>
2018-02-18 12:46:41 +0900

    mui-beta34

--- components/UserPage.js
+++ components/UserPage.js
@@ -80,7 +80,7 @@ export default class UserPage extends React.Component {
                 <p style={{margin:10, color:primary[500]}}>{'名前:' + user.name.first + ' ' + user.name.last} </p>
                 <p style={{margin:10, color:secondary[500]}}>{'性別:' + (user.gender == 'male' ? '男性' : '女性')}</p>
                 <div style={{textAlign: 'right'}} >
-                  <Button raised color='accent' onClick={() => this.handleClickOpen(user)}><Email style={{marginRight: 5, color: orange[200]}}/>メールする</Button>
+                  <Button variant='raised' color='secondary' onClick={() => this.handleClickOpen(user)}><Email style={{marginRight: 5, color: orange[200]}}/>メールする</Button>
                 </div>
               </CardContent>
             </Card>
