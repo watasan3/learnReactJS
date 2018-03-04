344a138f4ab6c8335e214462e433067178394b12
teradonburi <daikiterai@gmail.com>
2018-03-04 10:35:31 +0900

    webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
+  mode: 'development', // 開発モード
   devtool: 'inline-source-map', // ソースマップファイル追加 
   entry: './index.js', // エントリポイントのjsxファイル
   output: {
     filename: 'bundle.js' // 出力するファイル
   },
   module: {
-    loaders: [{
+    rules: [{
       test: /\.js?$/, // 拡張子がjsで
       exclude: /node_modules/, // node_modulesフォルダ配下は除外
-      loader: 'babel-loader', // babel-loaderを使って変換する
-      query: {
-        plugins: ['transform-react-jsx','transform-class-properties','babel-plugin-transform-decorators-legacy']
+      use: {
+        loader: 'babel-loader', // babel-loaderを使って変換する
       }
     }]
   }
