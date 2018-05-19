cf1ba985132d5ebe6bcfcfdd2aa00e338d5c84d9
teradonburi <daikiterai@gmail.com>
2018-05-20 00:33:50 +0900

    mui-v1

--- components/TodoPage.js
+++ components/TodoPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { AppBar, Toolbar, Button } from 'material-ui'
-import { withStyles } from 'material-ui/styles'
+import { AppBar, Toolbar, Button } from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
 
 @withStyles({
   root: {
