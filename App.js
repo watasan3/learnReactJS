d24aa101d643541aae16d38d4e04fb65c3ffa45f
teradonburi <daikiterai@gmail.com>
2018-09-25 18:12:32 +0900

    connected-react-router

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /*globals module: false */
 import React from 'react'
-import { ConnectedRouter as Router } from 'react-router-redux'
 import { Route, Switch } from 'react-router-dom'
+import { ConnectedRouter } from 'connected-react-router'
 import { hot } from 'react-hot-loader'
 
 import NotFound from './components/NotFound'
@@ -13,9 +13,9 @@ export default class App extends React.Component {
   render() {
     const { history } = this.props
     return (
-      <Router history={history}>
+      <ConnectedRouter history={history}>
         <Route component={AppRoute} />
-      </Router>
+      </ConnectedRouter>
     )
   }
 }
