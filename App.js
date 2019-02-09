cd3b8e41a820ebfdeb14db6b7980c9cbcd5654bc
teradonburi <daikiterai@gmail.com>
2019-02-10 00:37:42 +0900

    hook

--- App.js
+++ App.js
@@ -2,16 +2,23 @@ import React from 'react'
 import Counter from './Counter'
 import MultiCounter from './MultiCounter'
 import Timer from './Timer'
-
+import Increment from './Increment'
 
 export default class App extends React.Component {
 
   render () {
     return (
       <div>
+        <h1>useStateのサンプル</h1>
         <Counter />
         <MultiCounter />
+        <hr/>
+        <h1>useEffectのサンプル</h1>
         <Timer />
+        <hr/>
+        <h1>useReducer+useContextのサンプル</h1>
+        <Increment />
+        <hr/>
       </div>
     )
   }
--- MultiCounter.js
+++ MultiCounter.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const SuperButton = ({ onClick, children }) => {
-  const onClickHere = (e => {
+  const onClickFifth = (e => {
     // ワンクリックで５回クリック
     for (const _ of [0, 1, 2, 3, 4]) onClick(e)
   })
-  return <button onClick={onClickHere}>{children}</button>
+  return <button onClick={onClickFifth}>{children}</button>
 }
 
 const MultiCounter = () => {
@@ -14,12 +14,12 @@ const MultiCounter = () => {
   return (
     <div>
       <div>カウント：{state.count}</div>
-      {/* 値による変更、連続でレンダリングが走った場合にレンダリングの回数に合わせて、正しくインクリメントされない */}
-      {/* MultiCounterがレンダリングされたときのstateのため、5回連続で呼び出されてもcountは1だけ減ります */}
-      <SuperButton onClick={() => setState({count: state.count + 1})}>値による変更</SuperButton>
+      {/* 値による変更(非推奨)、連続でレンダリングが走った場合にレンダリングの回数に合わせて、正しくインクリメントされない */}
+      {/* SuperButtonに渡しているstateはMultiCounterがレンダリングされたときのstateのため、5回連続で呼び出されてもcountは1だけ増えます */}
+      <SuperButton onClick={() => setState({count: state.count + 1})}>値による変更+5（動作しない）</SuperButton>
       {/* 関数形式でパラメータの更新をすることもできる（推奨） */}
       {/* この場合は更新された値が都度引数として渡ってくるため、正常に+5される */}
-      <SuperButton onClick={() => setState(state => ({count: state.count + 1}))}>関数形式でのパラメータ変更</SuperButton>
+      <SuperButton onClick={() => setState(state => ({count: state.count + 1}))}>関数形式でのパラメータ変更+5</SuperButton>
     </div>
   )
 }
